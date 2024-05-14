e52b2e8d68f4e93b47df89709bb7e74a2ef24e9c
Souradeep20 <109914110+Souradeep20@users.noreply.github.com>
2024-05-14 09:56:59 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import LoginPage from './LoginPage';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <LoginPage />
+      </div>
+    </>
+  )
+}
+
+export default App
